e3595a1d94658029b1411ce8ca6163c8b9927703
unknown <flk09842@kloap.com>
2020-03-05 00:49:58 +0800

    rebuild toData

--- dist/decorator/property/mapping.d.ts
+++ dist/decorator/property/mapping.d.ts
@@ -1,2 +1,2 @@
 import { MappingVI } from '../../interface/mapping-vi.interface';
-export declare function Mapping(userSetting: MappingVI): (targetClassPrototype: any, propertyKey: any) => void;
+export declare function Mapping(userSetting?: MappingVI): (targetClassPrototype: any, propertyKey: any) => void;
---
+++ dist/util/to-data.d.ts
@@ -0,0 +1,1 @@
+export declare function toData(data: any): object;
